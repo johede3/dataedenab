c015d6affaac69ba86465aa0706d12a2e23e0ecf
Johannes <johede3@gmail.com>
2025-03-03 00:22:12 +0100

    fix tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,19 +72,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="sv-SE">
       <head>
-        {/* Google Tag Manager */}
         <Script
           id="gtm-script"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PQZ2G6QP');`,
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PQZ2G6QP');`,
           }}
         />
-        {/* End Google Tag Manager */}
 
         <link rel="preload" href="/images/BSHsmall.webp" as="image" type="image/webp" />
         {/* Schema Markup för Google */}
